7db269138c6dfbd269d48843d93e5c1c07f869d8
avilaa03 <lucasdeavila2003@gmail.com>
2024-10-31 13:11:07 -0300

    CSS Changes to Recipe Card Page.

--- recipes/views.py
+++ recipes/views.py
@@ -8,6 +8,6 @@ def home(request):
 
 def recipe(request, id):
     #return HTTP Response
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/recipe-view.html', context={
         'name': 'Lucas'
     })
