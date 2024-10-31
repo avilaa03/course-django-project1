f54750715005b910d68d9b094cc06cca72ef623a
avilaa03 <lucasdeavila2003@gmail.com>
2024-10-31 11:43:14 -0300

    New View and New Path with Dynamic Int ID

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from recipes.views import home
+from . import views
 # HTTP Request <- HTTP Response
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Lucas'
     })
+
+def recipe(request, id):
+    #return HTTP Response
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Lucas'
+    })
