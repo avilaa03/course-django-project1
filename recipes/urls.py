6ec3228550ef31ea1389aeb8a84fa144e268809a
avilaa03 <lucasdeavila2003@gmail.com>
2024-10-30 21:23:34 -0300

    CSS, HTML and Pattern changes and improvements

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from recipes.views import home, sobre, contato
+from recipes.views import home
 # HTTP Request <- HTTP Response
 
 urlpatterns = [
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     #return HTTP Response
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Lucas'
     })
-
-def contato(request):
-    return HttpResponse("CONTATO")
-
-def sobre(request):
-    return HttpResponse("SOBRE")
