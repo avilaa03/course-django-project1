975b63b3d473e45a01e89a08d01b24651a502d04
avilaa03 <lucasdeavila2003@gmail.com>
2024-11-01 14:11:42 -0300

    Added Dynamic Category Title Name and Error Response for 404

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe    
 from recipes.models import Recipe
+from django.http import Http404
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
@@ -11,10 +12,14 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not Found 😒')
     #return HTTP Response
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes
-    })
+        'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | ' 
+    })    
 
 def recipe(request, id):
     #return HTTP Response
